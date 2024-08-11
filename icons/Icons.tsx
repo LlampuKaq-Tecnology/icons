f58030863b2b20fd488aa2fea9f2f225cbd9e361
Luis <luisgarrido0987@gmail.com>
2024-08-10 20:48:04 -0500

    new chnages in refrest icons

--- icons/Icons.tsx
+++ icons/Icons.tsx
@@ -4,25 +4,28 @@ import { IconsProps, useIcons } from ".";
 function Icons({ size = 24, icon, className, stroke = 2 }: IconsProps) {
   const icons = useIcons();
   const [svg, setSvg] = useState<any>();
+
   const svgUrl = `https://icons.llampukaq.com/${icon}.svg`;
-  const res = useCallback(async () => {
-    try {
-      const response = await fetch(svgUrl);
-      const iconContent = await response.text();
-      const parser = new DOMParser();
-      const svgDoc = parser.parseFromString(iconContent, "image/svg+xml");
-      const svgElement = svgDoc.querySelector(".icon");
-      if (svgElement) {
-        svgElement.setAttribute("width", size.toString());
-        svgElement.setAttribute("height", size.toString());
-        svgElement.setAttribute("stroke-width", stroke.toString());
-        svgElement.removeAttribute("stroke");
-        setSvg(svgElement.outerHTML);
+  const res = async () => {
+    if (svg == undefined) {
+      try {
+        const response = await fetch(svgUrl);
+        const iconContent = await response.text();
+        const parser = new DOMParser();
+        const svgDoc = parser.parseFromString(iconContent, "image/svg+xml");
+        const svgElement = svgDoc.querySelector(".icon");
+        if (svgElement) {
+          svgElement.setAttribute("width", size.toString());
+          svgElement.setAttribute("height", size.toString());
+          svgElement.setAttribute("stroke-width", stroke.toString());
+          svgElement.removeAttribute("stroke");
+          setSvg(svgElement.outerHTML);
+        }
+      } catch (error) {
+        console.error("Error fetching icon:", error);
       }
-    } catch (error) {
-      console.error("Error fetching icon:", error);
     }
-  }, []);
+  };
 
   useEffect(() => {
     res();
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,27 +2,18 @@ import { Icons } from "@/icons";
 import { useState } from "react";
 
 export default function Home() {
-  const [show, setShow] = useState(false);
+  const [state, setState] = useState(false);
   return (
     <div className=" bg-zinc-900 flex h-screen hover:text-yellow-500 stroke-slate-500">
+      <Icons className="stroke-white" stroke={1} size={40} icon="Icon123" />
+      <Icons className="" size={30} icon="Icon123" />
       <button
         onClick={() => {
-          setShow(!show);
+          setState(!state);
         }}
       >
-        show
+        Change state
       </button>
-      {show && (
-        <>
-          <Icons
-            className="stroke-white"
-            stroke={1}
-            size={40}
-            icon="Icon123"
-          />
-          <Icons size={30} icon="Icon123" />
-        </>
-      )}
     </div>
   );
 }
